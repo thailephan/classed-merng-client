25d076b30de501c7c89536d34cff39f0af66a263
thailephan <thailephanminh@gmail.com>
2021-03-14 06:51:07 +0700

    Fix proxy and ApolloProvider

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -8,7 +8,7 @@ import { setContext } from 'apollo-link-context';
 import App from './App';
 
 const httpLink = createHttpLink({
-	uri: 'http://localhost:5000',
+	uri: 'https://serene-basin-32210.herokuapp.com/',
 });
 
 const authLink = setContext(() => {
